762c605200946d664da34e65f91f8e2e1e7954df
Reddzik <mateusz.redzik@o2.pl>
2020-05-24 22:00:16 +0200

    Added Db Service

---
+++ APBD10/APBD10/Services/EfPharmacyDbService.cs
@@ -0,0 +1,21 @@
+﻿using APBD10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD10.Services
+{
+    public class EfPharmacyDbService : IPharmacyDbService
+    {
+        private readonly PeopleDbContext _context;
+        public EfPharmacyDbService(PeopleDbContext context)
+        {
+            this._context = context;
+        }
+        public IEnumerable<Doctor> GetDoctors()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ APBD10/APBD10/Services/IPharmacyDbService.cs
@@ -0,0 +1,13 @@
+﻿using APBD10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD10.Services
+{
+    interface IPharmacyDbService
+    {
+        public IEnumerable<Doctor> GetDoctors(); 
+    }
+}
