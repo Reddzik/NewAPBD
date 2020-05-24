c4ec2baf7eb9fa41806fa1991f941c654f3b7e2a
Reddzik <mateusz.redzik@o2.pl>
2020-05-24 21:46:48 +0200

    Added new Tables and relations

---
+++ APBD10/APBD10/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APBD10.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD10.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PeopleDbContext _context;
+        public PeopleController(PeopleDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPeople()
+        {
+            return Ok(_context.Persons.ToList());
+        }
+    }
+}
---
+++ APBD10/APBD10/Models/PeopleDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD10.Models
+{
+    public class PeopleDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public PeopleDbContext() { }
+        public PeopleDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
